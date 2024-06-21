acce1b30ea86beeae44e0cbc9e63ac8a1b5f5e3b
Houcine Walaq <houcine.walaq@gmai.com>
2024-06-21 16:09:34 +0100

    adding book model

---
+++ backend/models/book.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const bookSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  publishedDate: {
+    type: Date,
+    required: true,
+},
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+
+const Book = mongoose.model('Book', bookSchema);
+
+export default Book;
