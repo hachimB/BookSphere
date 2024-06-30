09e474d2f5e51e88b2f5c575b3f2f7f00694a4f6
Houcine Walaq <houcine.walaq@gmai.com>
2024-06-30 17:00:13 +0100

    import style

--- frontend/book-app/src/components/ReadBook.js
+++ frontend/book-app/src/components/ReadBook.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import '../styles/ReadBook.css';
 
 function ReadBook() {
   const { bookId } = useParams();
